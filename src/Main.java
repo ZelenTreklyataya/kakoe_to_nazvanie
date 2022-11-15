70f08af85a2ac657d3c4039770038bf44a7a8135
ZelenTreklyaytaya <mr.greenmlg@yandex.ru>
2022-11-15 20:49:18 +0600

    comment

---
+++ src/Main.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+public class Main  {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Phone phone1 = new Phone("Samsung",scan.nextLine(),scan.nextLine(),"Black",170,6.6f,167.5f,67.4f,8,128);
+//        Phone phone1 = new Phone("Samsung","77771112345","Richard","Black",170,6.6f,167.5f,67.4f,8,128);
+        Phone phone2 = new Phone("Iphone","77007898778","Gleb","Gold",174,6.1f,170.5f,68.5f,6,256);
+        Phone phone3 = new Phone("Xiaomi","77052340000","Ivan","Silver",160,5.6f,150.6f,57.7f,4,64);
+        Phone phone4 = new Phone("OPPO","77057678900","Oleg","Black",180,7f,180.5f,70.8f,8,512);
+        Phone phone5 = new Phone("Xiaomi","77758885678","Daniel","Gold",165,6.1f,164.8f,62.6f,8,64);
+        System.out.print(phone1.model);
+        test(phone1.model);
+        System.out.print(phone1.number);
+        test(phone1.number);
+        System.out.print(phone1.ownerName);
+        test(phone1.ownerName);
+        System.out.print(phone1.color);
+        test(phone1.color);
+        System.out.print(phone1.weight);
+        test(phone1.weight);
+        System.out.print(phone1.size);
+        test(phone1.size);
+        System.out.print(phone1.height);
+        test(phone1.height);
+        System.out.print(phone1.width);
+        test(phone1.width);
+        System.out.print(phone1.CPUcores);
+        test(phone1.CPUcores);
+        phone1.receiveCall();
+        phone2.receiveCall();
+        phone3.receiveCall();
+        Empty newEmpty = new Empty();
+    }
+    public static class Phone {
+        private String model;
+        private String number;
+        private String ownerName;
+        private String color;
+        private int weight;
+        private float size;
+        private float height;
+        private float width;
+        private int CPUcores;
+        private int memory;
+        private int numOfCamera;
+        private String colorCase;
+        private boolean scanner;
+        public Phone(String model, String number, String ownerName, String color, int weight, float size, float height, float width, int CPUcores, int memory) {
+            this.model = model;
+            this.number = number;
+            this.ownerName = ownerName;
+            this.color = color;
+            this.weight = weight;
+            this.size = size;
+            this.height = height;
+            this.width = width;
+            this.CPUcores = CPUcores;
+            this.memory = memory;
+            new Phone(2, "Black", true);
+        }
+        public Phone(int numOfCamera, String colorCase, boolean scanner){
+            this.numOfCamera = numOfCamera;
+            this.colorCase = colorCase;
+            this.scanner = scanner;
+        }
+
+
+        public void receiveCall() {
+            System.out.println(getOwnerName());
+            System.out.println(getNumber());
+        }
+
+        public String getOwnerName() {
+            return ownerName;
+        }
+
+        public String getNumber() {
+            return number;
+        }
+
+    }
+    public static void test(Object value){
+        if(value instanceof Integer){
+            System.out.println(" Это Integer ");
+        }
+        else if(value instanceof String){
+            System.out.println(" Это String ");
+        }
+        else if(value instanceof Float){
+            System.out.println(" Это Float ");
+        }
+    }
+    public static class Empty{
+        public Empty() {
+
+        }
+    }
+}
